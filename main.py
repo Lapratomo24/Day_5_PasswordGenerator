ae675f37f1a9baa0c942de7a6d66ff00c08d93f5
Aditya <lapratomo24@yahoo.com>
2024-07-14 00:16:57 +0200

    randomized solution

--- main.py
+++ main.py
@@ -13,15 +13,17 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_numbers = int(input("How many numbers would you like?\n"))
 nr_symbols = int(input("How many symbols would you like?\n"))
 
-password = ""
+password = []
 
 for letter in range(1, nr_letters + 1):
-    password += random.choice(letters)
+    password.append(random.choice(letters))
 
-for number in range(1, nr_numbers):
-    password += random.choice(numbers)
+for number in range(1, nr_numbers + 1):
+    password.append(random.choice(numbers))
 
-for symbol in range(1, nr_symbols):
-    password += random.choice(symbols)
+for symbol in range(1, nr_symbols + 1):
+    password.append(random.choice(symbols))
     
+random.shuffle(password)
+password = "".join(password)
 print(f"Your new password is {password}")
